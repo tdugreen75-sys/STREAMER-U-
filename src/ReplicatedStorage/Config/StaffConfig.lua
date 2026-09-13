8adbaee1e3f4b02213d6894017304a7ce0fa6e85
tdugreen75-sys <tdugreen75@gmail.com>
2026-09-12 19:35:23 -0500

    Add fixed staff identity config

---
+++ src/ReplicatedStorage/Config/StaffConfig.lua
@@ -0,0 +1,9 @@
+return {
+    IdPrefix = "STF",
+    IdNumberLength = 7,
+    IdSalt = 731429,
+    DefaultDisplayTitle = "Staff",
+    AuthorizedUserIds = {
+        -- [1234567] = true,
+    },
+}
