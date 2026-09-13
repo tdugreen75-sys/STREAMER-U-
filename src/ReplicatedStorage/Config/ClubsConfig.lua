ca0d55d56f2772eb45487b6b93a90fb6c2cb5ee5
tdugreen75-sys <tdugreen75@gmail.com>
2026-09-12 19:35:25 -0500

    Add Phase 1 clubs config

---
+++ src/ReplicatedStorage/Config/ClubsConfig.lua
@@ -0,0 +1,12 @@
+return {
+    Clubs = {
+        "Fraternity",
+        "Basketball",
+        "Cheerleading",
+        "Film",
+        "Science & Engineering",
+        "Musical Arts",
+        "Debate",
+        "Drama",
+    },
+}
